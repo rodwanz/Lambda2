9a5a91ed02175f4f02c543ff6e3b6c6f13ed92fd
Rodrigo Wanzeler <wanzervai@gmail.com>
2021-12-28 09:17:58 -0300

    Inline lambada expression

--- src/application/ProgramaConsumer.java
+++ src/application/ProgramaConsumer.java
@@ -21,10 +21,7 @@ public class ProgramaConsumer {
 		
 		double factor = 1.1;
 		
-		Consumer<Product> cons = p ->{
-			p.setPrice(p.getPrice() * factor);
-		};
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 		list.forEach(System.out::println);
 
 	}
