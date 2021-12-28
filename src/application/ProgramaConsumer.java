363106e482b5ac608638586e175faf8c479ada90
Rodrigo Wanzeler <wanzervai@gmail.com>
2021-12-28 08:54:47 -0300

    Method reference static

--- src/application/ProgramaConsumer.java
+++ src/application/ProgramaConsumer.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Locale;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class ProgramaConsumer {
 
@@ -19,7 +18,7 @@ public class ProgramaConsumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(new PriceUpdate());
+		list.forEach(Product::staticPriceUpdate);
 		list.forEach(System.out::println);
 
 	}
